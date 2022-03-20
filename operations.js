22f9b5ec2916cd981e2b50f732769272189fef5d
ryliewalsh <ryliewalsh@u.boisestate.edu>
2022-03-20 16:18:28 -0600

    fix #1

--- operations.js
+++ operations.js
@@ -3,8 +3,13 @@ function sum(a, b)
     return a + b;
 }
 
+function subtract(a,b)
+{
+    return a - b;
+}
 
 module.exports = {
     sum,
-    //subtract, //TODO: uncomment this line to make the subtract function available in other files
+    subtract,
+    
 };
--- operations.test.js
+++ operations.test.js
@@ -15,3 +15,15 @@ test('adds 0 + 4 to equal 4', () => {
 test('adds -1 + (-7) to equal -8', () => {
     expect(sum(-1, -7)).toBe(-8)
 });
+
+test('subtracts 6 - 2 to equal 4', () => {
+    expect(subtract(6, 2)).toBe(4)
+});
+
+test('subtracts 10 - 10 to equal 0', () => {
+    expect(subtract(10, 10)).toBe(0)
+});
+
+test('subtracts -1 - (-2) to equal 1', () => {
+    expect(subtract(-1, -2)).toBe(1)
+});
